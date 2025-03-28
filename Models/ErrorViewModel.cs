283938680048bd4f54e8547f86f59dfe2fd1bb23
TsiraM <mamaladze.ts@gmail.com>
2025-03-27 21:54:47 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TicketHubApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
